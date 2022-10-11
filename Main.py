1b2a1355b5808f9f77d1550844762d65d5a2c4c7
isaac-joshua <110010536+isaac-joshua@users.noreply.github.com>
2022-10-11 12:10:41 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,35 +1,32 @@
 from typing import List
 
-def merge(nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-  l1 = nums1[:]
-  l2 = nums2[:]
-  curr, i, j = 0, 0, 0
-  while i < m and j < n:
-    if l1[i] < l2[j]:
-      nums1[curr] = l1[i]
-      i += 1
-    else:
-      nums1[curr] = l2[j]
-      j += 1
-    curr += 1
-  while i < m:
-    nums1[curr] = l1[i]
-    curr += 1
-    i += 1
-  while j < n:
-    nums1[curr] = l2[j]
-    curr += 1
-    j += 1
+# divide function
+def partition(arr,low,high):
+   i = ( low-1 )
+   pivot = arr[high] # pivot element
+   for j in range(low , high):
+      # If current element is smaller
+      if arr[j] <= pivot:
+         # increment
+         i = i+1
+         arr[i],arr[j] = arr[j],arr[i]
+   arr[i+1],arr[high] = arr[high],arr[i+1]
+   return ( i+1 )
+# sort
+def quick_sort(arr,low,high):
+   if low < high:
+      # index
+      pi = partition(arr,low,high)
+      # sort the partitions
+      quick_sort(arr, low, pi-1)
+      quick_sort(arr, pi+1, high)
+   return arr
 
-
-# Do not change the following code
-nums1 = []
-nums2 = []
-for item in input().split(', '):
-  nums1.append(int(item))
-for item in input().split(', '):
-  nums2.append(int(item))
-m = int(input())
-n = int(input())
-merge(nums1, m, nums2, n)
-print(nums1)
+input_data = input()
+data = []
+for item in input_data.split(', '):
+  if item.isnumeric():
+    data.append(int(item))
+  elif item.lstrip("-").isnumeric():
+    data.append(int(item))
+print(quick_sort(data, 0, len(data)-1))
